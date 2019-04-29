973306e2a677e7a70a4724c6887a5b60b6c75b66
Jon Ayers <jon@coder.com>
2019-04-29 14:17:14 -0500

    Add CI

---
+++ lint.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -euo pipefail
+
+files=$(gofmt -l -s .)
+
+if [ ! -z "$files" ]; 
+then
+  echo "The following files need to be formatted:"
+  echo "$files"
+  echo "Please run 'gofmt -w -s .'"
+  exit 1
+fi
+
+go vet -composites=false .
